4c494a85da1f3328a5aab48055ab6df9d841d2d4
jyotisman1207 <jyotisman.sahoo1229@gmail.com>
2026-02-10 17:50:38 +0530

    DAY 1 - Adding image loading experiment

--- code_read_image.py
+++ code_read_image.py
@@ -1,1 +1,11 @@
+from PIL import Image
 
+img = Image.open("sample.jpg")
+
+print("Image format:", img.format)
+print("Image size (width, height):", img.size)
+print("Color mode:", img.mode)
+
+
+width, height = img.size
+print("Total pixels:", width * height)
