dfdc60fee2778fc1576f48074d6d6003b8c5f769
jyotisman1207 <jyotisman.sahoo1229@gmail.com>
2026-04-11 12:05:44 +0530

    Refactor atmospheric light estimation code
    Removed comments and simplified atmospheric light estimation.

--- experiments/atmospheric_light.py
+++ experiments/atmospheric_light.py
@@ -1,13 +1,11 @@
 import cv2
 import numpy as np
 
-# Load image
+
 img = cv2.imread("sample.jpg")
 
-# Compute dark channel
 dark = np.min(img, axis=2)
 
-# Estimate atmospheric lightnsjsn
 A = np.max(dark)
 
 print("Estimated atmospheric light:", A)
