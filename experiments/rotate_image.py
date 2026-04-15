3b77c5c460914559ecf5442a1c275be00e5a289f
jyotisman1207 <jyotisman.sahoo1229@gmail.com>
2026-04-16 00:22:29 +0530

    rotate_image.py

--- experiments/rotate_image.py
+++ experiments/rotate_image.py
@@ -1,12 +1,9 @@
 from PIL import Image
 
-# Load image
 img = Image.open("sample.jpg")
 
-# Rotate image by 90 degrees
 rotated = img.rotate(90)
 
-# Save rotated image
 rotated.save("rotated_image.jpg")
 
 print("Image rotation completed")
