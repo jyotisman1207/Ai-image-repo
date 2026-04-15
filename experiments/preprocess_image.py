4573024056f89302fe4f2760afac335256456b0e
jyotisman1207 <jyotisman.sahoo1229@gmail.com>
2026-04-16 00:23:56 +0530

    image preprocessing code
    Removed comments and streamlined image preprocessing steps.

--- experiments/preprocess_image.py
+++ experiments/preprocess_image.py
@@ -1,15 +1,10 @@
 from PIL import Image
 
-# Load image
 img = Image.open("sample.jpg")
-
-# Resize image
 resized = img.resize((256, 256))
 
-# Convert to grayscale
 gray = img.convert("L")
 
-# Save processed images
 resized.save("resized_image.jpg")
 gray.save("grayscale_image.jpg")
 
