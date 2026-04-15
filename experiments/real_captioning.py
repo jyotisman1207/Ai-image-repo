6de7eca706b310c91dd80e1b7d76003adfe74f5e
jyotisman1207 <jyotisman.sahoo1229@gmail.com>
2026-04-16 00:22:55 +0530

    real_captioning.py to simplify code
    Removed comments and streamlined the code for image captioning.

--- experiments/real_captioning.py
+++ experiments/real_captioning.py
@@ -1,13 +1,10 @@
 from transformers import pipeline
 from PIL import Image
 
-# Load captioning pipeline
 captioner = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
 
-# Load image
 image = Image.open("sample.jpg")
 
-# Generate caption
 result = captioner(image)
 
 print("Generated Caption:", result[0]['generated_text'])
