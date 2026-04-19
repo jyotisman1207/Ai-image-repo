96eb269e6d138014750da92ba91dd7433e3acfb4
jyotisman1207 <jyotisman.sahoo1229@gmail.com>
2026-04-19 23:37:08 +0530

    Clean up comments in image histogram script
    Removed comments for a cleaner code presentation.

--- experiments/image_histogram.py
+++ experiments/image_histogram.py
@@ -2,16 +2,14 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load image
+
 img = Image.open("sample.jpg")
 
-# Convert to grayscale
+
 gray = img.convert("L")
 
-# Convert to numpy array
 img_array = np.array(gray)
 
-# Plot histogram
 plt.hist(img_array.flatten(), bins=256, range=[0,256])
 plt.title("Image Histogram")
 plt.xlabel("Pixel Intensity")
