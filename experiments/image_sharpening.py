07118d2fbb794c9345d538b74bfb8b1fded848aa
jyotisman1207 <jyotisman.sahoo1229@gmail.com>
2026-04-19 23:39:09 +0530

    Fix image sharpening script

--- experiments/image_sharpening.py
+++ experiments/image_sharpening.py
@@ -1,18 +1,15 @@
 import cv2
 import numpy as np
 
-# Load image
 img = cv2.imread("sample.jpg")
 
-# Sharpening kernel
 kernel = np.array([[0,-1,0],
                    [-1,5,-1],
                    [0,-1,0]])
 
-# Apply sharpening
+
 sharpened = cv2.filter2D(img, -1, kernel)
 
-# Save result
 cv2.imwrite("sharpened_image.jpg", sharpened)
 
 print("Image sharpening applied")
