3b8d867e9c66acbb6ed92580111b84735aa5946e
jyotisman1207 <jyotisman.sahoo1229@gmail.com>
2026-05-01 21:22:12 +0530

    image denoising script

--- experiments/image_denoising.py
+++ experiments/image_denoising.py
@@ -1,12 +1,9 @@
 import cv2
 
-# Load image
 img = cv2.imread("sample.jpg")
 
-# Apply Gaussian Blur to remove noise
 denoised = cv2.GaussianBlur(img, (5, 5), 0)
 
-# Save result
 cv2.imwrite("denoised_image.jpg", denoised)
 
 print("Image denoising completed")
