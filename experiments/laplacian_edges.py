14deff06021a9ca0339f5f9373fa6e3acb31e7c2
jyotisman1207 <jyotisman.sahoo1229@gmail.com>
2026-05-11 22:01:51 +0530

    Remove comments and streamline Laplacian edge detection

--- experiments/laplacian_edges.py
+++ experiments/laplacian_edges.py
@@ -1,14 +1,11 @@
 import cv2
 import numpy as np
 
-# Load grayscale image
 img = cv2.imread("sample.jpg", 0)
 
-# Apply Laplacian
 laplacian = cv2.Laplacian(img, cv2.CV_64F)
 laplacian = np.uint8(np.absolute(laplacian))
 
-# Save result
 cv2.imwrite("laplacian_edges.jpg", laplacian)
 
 print("Laplacian edge detection applied")
